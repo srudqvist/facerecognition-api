f0fb90d11c51a553ae2f0857f30dbdcf369bfc51
Sam <60146956+srudqvist@users.noreply.github.com>
2023-06-25 23:37:44 -0500

    changed res for signin

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.post("/signin", (req, res) => {
 		req.body.email === database.users[0].email &&
 		req.body.password === database.users[0].password
 	) {
-		res.json("Success");
+		res.json(database.users[0]);
 	} else {
 		res.status(400).json("Error Logging In");
 	}
