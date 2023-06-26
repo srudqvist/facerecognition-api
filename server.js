48d08724b6da26e3d097aad32bba3b0941e765d0
Sam <60146956+srudqvist@users.noreply.github.com>
2023-06-25 23:05:02 -0500

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3001;
 
